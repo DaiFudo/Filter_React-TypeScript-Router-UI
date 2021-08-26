e6e05dedee55f45f8be34569f274c398abf4ea08
Dev <dev@Mac-mini-Dev.local>
2021-08-26 14:17:48 +0300

    add design

--- src/components/app/SearchPanel.tsx
+++ src/components/app/SearchPanel.tsx
@@ -1,4 +1,37 @@
 import React, { useState, MouseEvent } from "react";
+import { Button, Box, TextField, styled } from "@material-ui/core";
+
+const InputStyle = styled(TextField)({
+  fontSize: "2rem",
+  border: "2px solid grey",
+  borderRadius: "10px",
+  padding: "10px",
+  backgroundColor: "white",
+});
+
+const BoxStyle = styled(Box)({
+  textTransform: "uppercase",
+  paddingTop: "50px",
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  textAlign: "center",
+  fontSize: "2rem",
+  padding: "10px",
+  width: "350px",
+  height: "350px",
+});
+
+const MyButton = styled(Button)({
+  marginTop: "10px",
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  color: "white",
+  height: 48,
+  padding: "0 30px",
+  display: "flex",
+  margin: "0 auto",
+});
+
 interface SearchPanelProps {
   onAdd(request: string): void;
 }
@@ -20,12 +53,11 @@ export const SearchPanel: React.FC<SearchPanelProps> = (props) => {
     e.preventDefault();
     props.onAdd(request);
     setRequest("");
-    console.log(request, "from SearchPanel request");
   };
 
   return (
-    <form className="app-form">
-      <input
+    <BoxStyle className="app-form">
+      <InputStyle
         onChange={changeHandler}
         onKeyPress={keyPressHandler}
         value={request}
@@ -34,9 +66,9 @@ export const SearchPanel: React.FC<SearchPanelProps> = (props) => {
         id="title"
         className="app-input"
       />
-      <button onClick={changeHandlerBtn} type="submit" className="btn">
-        Search
-      </button>
-    </form>
+      <MyButton onClick={changeHandlerBtn} type="submit" className="btn">
+        Поиск
+      </MyButton>
+    </BoxStyle>
   );
 };
--- src/router.tsx
+++ src/router.tsx
@@ -6,14 +6,24 @@ import { Nickname } from "./components/app/Nickname";
 import { RealName } from "./components/app/RealName";
 import { Phone } from "./components/app/Phone";
 import { Route, Switch, useHistory } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { styled } from "@material-ui/core/styles";
+
+const StyleContainer = styled(Container)({
+  backgroundColor: "#cfe8fc",
+  height: "100vh",
+  position: "absolute",
+});
 
 export const App: React.FC = () => {
   const history = useHistory();
   const addHandler = (request: string) => {
-    console.log(request, "from App request");
-
     const regNickname = new RegExp(/^[^0-9]\w+$/),
       regName = new RegExp(/^[a-z ,.'-]+$/i),
+      regPhone = new RegExp(
+        /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+      ),
       regMail = new RegExp(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       ),
@@ -22,7 +32,6 @@ export const App: React.FC = () => {
       );
 
     if (regNickname.test(request) === true) {
-      console.log("is it Nickname");
       history.push("/Nickname");
     } else if (regMail.test(request) === true) {
       history.push("/Mail");
@@ -30,19 +39,30 @@ export const App: React.FC = () => {
       history.push("/Name");
     } else if (regIp.test(request) === true) {
       history.push("/Ip");
+    } else if (regPhone.test(request) === true) {
+      history.push("/Phone");
     } else {
-      console.log("Write normal text");
-    } // запилить обычную константу с данным
+      history.push("/");
+    }
   };
 
   return (
-    <Switch>
-      <Route path="/" render={() => <SearchPanel onAdd={addHandler} />} exact />
-      <Route path="/Nickname" component={Nickname} />;
-      <Route path="/Ip" component={Ip} />
-      <Route path="/Mail" component={Mail} />
-      <Route path="/Phone" component={Phone} />
-      <Route path="/Name" component={RealName} />
-    </Switch>
+    <>
+      <CssBaseline />
+      <StyleContainer maxWidth="xl">
+        <Switch>
+          <Route
+            path="/"
+            render={() => <SearchPanel onAdd={addHandler} />}
+            exact
+          />
+          <Route path="/Nickname" component={Nickname} />;
+          <Route path="/Ip" component={Ip} />
+          <Route path="/Mail" component={Mail} />
+          <Route path="/Phone" component={Phone} />
+          <Route path="/Name" component={RealName} />
+        </Switch>
+      </StyleContainer>
+    </>
   );
 };
