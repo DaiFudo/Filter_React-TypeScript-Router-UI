813ec163a21614b45e112a6812fcfa7c4458abea
Ilya Zaichenko <dai.fudo@mail.ru>
2021-08-26 16:52:28 +0300

    copy

---
+++ src copy/components/app/Ip.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Button, Box, styled } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const BoxStyle = styled(Box)({
+  textTransform: "uppercase",
+  paddingTop: "50px",
+  margin: "0 auto",
+  display: "flex",
+  flexDirection: "column",
+  textAlign: "center",
+  fontSize: "2rem",
+  padding: "10px",
+  minWidth: "350px",
+  height: "350px",
+});
+
+const LinkRout = styled(Link)({
+  margin: "0 auto",
+  display: "flex",
+  textDecoration: "none",
+});
+
+const MyButton = styled(Button)({
+  marginTop: "10px",
+  background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+  color: "white",
+  height: 48,
+  padding: "0 30px",
+  display: "flex",
+  margin: "0 auto",
+});
+
+export const Ip: React.FC = () => {
+  return (
+    <BoxStyle>
+      Мы найдем Вас по IP!
+      <LinkRout to="/">
+        <MyButton variant="contained" color="primary">
+          Спрятаться под покрывало
+        </MyButton>
+      </LinkRout>
+    </BoxStyle>
+  );
+};
---
+++ src copy/router.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { SearchPanel } from "./components/app/SearchPanel";
+import { Ip } from "./components/app/Ip";
+import { Mail } from "./components/app/Mail";
+import { Nickname } from "./components/app/Nickname";
+import { RealName } from "./components/app/RealName";
+import { Phone } from "./components/app/Phone";
+import { Route, Switch, useHistory } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { styled } from "@material-ui/core/styles";
+
+const StyleContainer = styled(Container)({
+  backgroundColor: "#cfe8fc",
+  height: "100vh",
+  position: "absolute",
+});
+
+export const App: React.FC = () => {
+  const history = useHistory();
+  const addHandler = (request: string) => {
+    const regNickname = new RegExp(/^[^0-9]\w+$/),
+      regName = new RegExp(/^[a-z ,.'-]+$/i),
+      regPhone = new RegExp(
+        /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
+      ),
+      regMail = new RegExp(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      ),
+      regIp = new RegExp(
+        /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+      );
+
+    if (regNickname.test(request) === true) {
+      history.push("/Nickname");
+    } else if (regMail.test(request) === true) {
+      history.push("/Mail");
+    } else if (regName.test(request) === true) {
+      history.push("/Name");
+    } else if (regIp.test(request) === true) {
+      history.push("/Ip");
+    } else if (regPhone.test(request) === true) {
+      history.push("/Phone");
+    } else {
+      history.push("/");
+    }
+  };
+
+  return (
+    <>
+      <CssBaseline />
+      <StyleContainer maxWidth="xl">
+        <Switch>
+          <Route
+            path="/"
+            render={() => <SearchPanel onAdd={addHandler} />}
+            exact
+          />
+          <Route path="/Nickname" component={Nickname} />;
+          <Route path="/Ip" component={Ip} />
+          <Route path="/Mail" component={Mail} />
+          <Route path="/Phone" component={Phone} />
+          <Route path="/Name" component={RealName} />
+        </Switch>
+      </StyleContainer>
+    </>
+  );
+};
